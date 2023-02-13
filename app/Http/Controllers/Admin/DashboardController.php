226a2084c1ccdaf3433b25f1fa96356d28fd8a3e
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-02-13 15:40:07 +0100

    initial commit

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Type;
+
+class DashboardController extends Controller
+{
+    public  function home() {
+
+        // $user = Auth::user();
+        // $users = User::all();
+
+        $users = User::all();
+
+        $projects = Project::all();
+
+        $types = Type::all();
+
+        return view("dashboard", compact("users", "projects", "types"));
+    }
+}
